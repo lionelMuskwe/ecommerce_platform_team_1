a98470f241f9454693bea30aca90f89bb08b86e9
Onno Tsang <onno7@outlook.com>
2023-02-08 22:50:51 +0000

    added files for product database

---
+++ ecommerce-platform/database/migrations/2023_02_08_223058_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->tinyText('description');
+            $table->string('size');
+            $table->double('price');
+            $table->Integer('inventory_id');
+            $table->Integer('category_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
