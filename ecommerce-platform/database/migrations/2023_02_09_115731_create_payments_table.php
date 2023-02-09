31a3b1414aa331515e25ebb0cac522b4fe46a83f
Onno Tsang <onno7@outlook.com>
2023-02-09 12:01:42 +0000

    added a table called payments to the database

---
+++ ecommerce-platform/database/migrations/2023_02_09_115731_create_payments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->foreignId('user_id');
+            $table->string('payment_type');
+            $table->string('account_number');
+            $table->string('sort_code');
+            $table->date('expiration_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+};
