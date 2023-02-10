29455b35925260a38ccadf9d299bb0f45261fc7d
Onno Tsang <onno7@outlook.com>
2023-02-10 13:37:25 +0000

    updated Product model

--- ecommerce-platform/app/Models/Product.php
+++ ecommerce-platform/app/Models/Product.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+
+        "name",
+        "description",
+        "size",
+        "price",
+        "image",
+        "inventory_id",
+        "category_id"
+    ];
 }
