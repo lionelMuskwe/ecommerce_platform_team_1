b4b7122cf3e5c8d8a91cac7f3ac939c308e45070
lionelMuskwe <lionel04muskwe@gmail.com>
2023-02-10 22:05:43 +0000

    Customer home page now displays

--- ecommerce-platform/resources/views/customers/homepage.blade.php
+++ ecommerce-platform/resources/views/customers/homepage.blade.php
@@ -1,1 +1,14 @@
-<h1>Application homepage</h1>
+@extends("../general/master")
+
+@section("title")
+    Home
+@endsection()
+
+@section("content")
+    {{-- Content goes in this section --}}
+    <h1>You content should go in this section</h1>
+    <br>
+    <p>Remember to always write clean code</p>
+
+@endsection()
+
