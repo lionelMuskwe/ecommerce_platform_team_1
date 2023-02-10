22aa116b9a023c7e16a55a3042ee4f1108c4cdd7
lionelMuskwe <lionel04muskwe@gmail.com>
2023-02-10 12:47:44 +0000

    Updated address model

--- ecommerce-platform/app/Models/Address.php
+++ ecommerce-platform/app/Models/Address.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Address extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        "address_line_1",
+        "address_line_2",
+        "city",
+        "postcode"
+    ];
+
 }
