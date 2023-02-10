7c677e7ce0d67055e14c758a4711ef235a934487
Onno Tsang <onno7@outlook.com>
2023-02-10 13:30:16 +0000

    updated Inventory model

--- ecommerce-platform/app/Models/Inventory.php
+++ ecommerce-platform/app/Models/Inventory.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Inventory extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+
+        "quantity",
+        "delivery_id"
+        
+    ];
 }
