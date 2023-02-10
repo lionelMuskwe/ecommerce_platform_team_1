b48cc7cbcd4599ffd877a652d312e67a0b32a0bc
lionelMuskwe <lionel04muskwe@gmail.com>
2023-02-10 12:47:54 +0000

    Updated payment model

--- ecommerce-platform/app/Models/Payment.php
+++ ecommerce-platform/app/Models/Payment.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Payment extends Model
 {
     use HasFactory;
+
+    protected $table ='product';
+    protected $fillable = [
+        "name",
+        "description",
+        "price",
+        "quantity",
+        "front_image",
+        "inventory_id",
+        "category_id"
+    ];
+
+
 }
