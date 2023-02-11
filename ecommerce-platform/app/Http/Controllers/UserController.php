9c21630314e838121ed133e8bff4752b037252fa
Onno Tsang <onno7@outlook.com>
2023-02-11 15:40:59 +0000

    added a controller called UserController.php

--- ecommerce-platform/app/Http/Controllers/UserController.php
+++ ecommerce-platform/app/Http/Controllers/UserController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     public function sign(Request $request){
-             return view("general/signup");
-        }
+        return view("general/signup");
+    }
+
+    public function log(Request $request){
+        return view("general/login");
+    }
 }
