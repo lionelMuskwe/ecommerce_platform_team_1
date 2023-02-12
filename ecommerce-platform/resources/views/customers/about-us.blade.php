e1895069f9bacbcee3dc2b6f548df9227b465fe9
David David-Aghaiyo <210186620@aston.ac.uk>
2023-02-12 19:43:23 +0000

    modified about us

--- ecommerce-platform/resources/views/customers/about-us.blade.php
+++ ecommerce-platform/resources/views/customers/about-us.blade.php
@@ -11,8 +11,11 @@
 @section("content")
 
     <div class="mission-statement">
-        Our company is about doing etc. Readers Republic is one of the only
-    stores that sells books in etc
+    We are a passionate team of bibliophiles who have come together to create a haven for book lovers. Our mission is to promote the joy of reading and to make books accessible to everyone. We believe that books have the power to inspire, educate, and entertain, and we are dedicated to providing a diverse range of books to suit every taste and interest.
+
+Our store is filled with a carefully curated selection of books, from the latest bestsellers to timeless classics. We also have a range of unique and handpicked merchandise, including bookish gifts and stationery, to make your reading experience even more enjoyable. Whether you are looking for your next great read or just want to spend some time surrounded by books, you will find a warm and welcoming atmosphere in our store.
+
+We are committed to providing excellent customer service, and our knowledgeable and friendly staff are always on hand to help you find the perfect book.
     </div>
 
     <p>We hope that we can help you in finding the write books etc</p>
