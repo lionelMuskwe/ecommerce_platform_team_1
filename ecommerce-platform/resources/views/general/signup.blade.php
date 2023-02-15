c873e38b3d971d6fe137e19907190dc4e2e4eb29
sajni4 <114992911+sajni4@users.noreply.github.com>
2023-02-15 10:52:56 +0000

    Edited sign up page

--- ecommerce-platform/resources/views/general/signup.blade.php
+++ ecommerce-platform/resources/views/general/signup.blade.php
@@ -10,18 +10,25 @@
 
 @section("content")
     {{-- Content goes in this section --}}
-    <h1>You content should go in this section</h1>
-    <br>
-    <p>Remember to always write clean code</p>
+    <h1>Make an account</h1>
 
     <form action="/signup" method="POST">
         @csrf
         <label for="firstname">First Name</label>
-        <input type="text" name="firstname"></br>
+        <input type="text" name="firstname" placeholder="First Name"></br>
         <label for="lastname">Last Name</label>
-        <input type="text" name="lastname"></br>
-        <button>Send</button>
+        <input type="text" name="lastname" placeholder="Second Name"></br>
+        <label for="email">Email</label>
+        <input type="text" name="email" placeholder="Email"></br>
+        <label for="password">Password</label>
+        <input type="text" name="password" placeholder="Password"></br>
+        <label for="c_password">Confirm Password</label>
+        <input type="text" name="c_password" placeholder="Password"></br><br>
+        <button>Register</button><br>
     </form>
 
+    <div class="bottom_section">
+        <p> Already have an account with us? Log in now! </p>
+    </div>
 @endsection()
 
