7142b5b08b5e28abc789ac0a7a44c9e6ed0c0cc8
Onno Tsang <onno7@outlook.com>
2023-02-18 10:13:44 +0000

    changed the first name input text field to username

--- ecommerce-platform/resources/views/general/login.blade.php
+++ ecommerce-platform/resources/views/general/login.blade.php
@@ -17,8 +17,8 @@
 
     <form action="/login" Method="POST">
         @csrf
-        <label for = "firstname">First Name</label>
-        <input type="text" name="firstname"></br>
+        <label for = "username">First Name</label>
+        <input type="text" name="username"></br>
         <label for = "password">Password</label>
         <input type="text" name="password"></br>
         <button>Login</button>
