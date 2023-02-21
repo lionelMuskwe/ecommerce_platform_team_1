8530897ff2428f1371d794a08e39f32e157d569d
Onno Tsang <onno7@outlook.com>
2023-02-21 20:33:28 +0000

    updated product function to give data of products from database

--- ecommerce-platform/app/Http/Controllers/ProductController.php
+++ ecommerce-platform/app/Http/Controllers/ProductController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
     public function product(Request $request){
-        return view("customers/multiple-products");
+        $data = Product::all();
+        return view("customers/multiple-products", ['products'=>$data]);
     }
 
     public function detailedProduct(Request $request){
