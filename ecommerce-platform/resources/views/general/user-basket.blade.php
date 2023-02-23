3ce15c2ca7de0b8df00910c41212f9002863ad4f
sajni4 <114992911+sajni4@users.noreply.github.com>
2023-02-23 09:49:11 +0000

    Added the user basket, made a basic user-basket page that shows a few items in the basket. Also created and added to the css stylesheet for the user basket.

--- ecommerce-platform/resources/views/general/user-basket.blade.php
+++ ecommerce-platform/resources/views/general/user-basket.blade.php
@@ -9,9 +9,78 @@
 @endsection
 
 @section("content")
-    {{-- Content goes in this section --}}
-    <h1>You content should go in this section</h1>
-    <br>
-    <p>Remember to always write clean code</p>
+    <div class="small-container cart-page">
+        <table>
+            <tr>
+                <th>Product</th>
+                <th>Quantity</th>
+                <th>Subtotal</th>
+            </tr>
+            <tr>
+                <td>
+                    <div class="cart-info">
+                        <img src="images/readers-republic-logo.png">
+                        <div>
+                            <p>Example Image</p>
+                            <small>Price: £5.00</small><br>
+                            <a href="">Remove</a>
+                        </div>
+                    </div>
+                </td>
+                <td><input type="number" value="1"></td>
+                <td>£5.00</td></td>
+            </tr>
+
+            <tr>
+                <td>
+                    <div class="cart-info">
+                        <img src="images/readers-republic-logo.png">
+                        <div>
+                            <p>Example Image</p>
+                            <small>Price: £10.00</small><br>
+                            <a href="">Remove</a>
+                        </div>
+                    </div>
+                </td>
+                <td><input type="number" value="1"></td>
+                <td>£10.00</td></td>
+            </tr>
+
+            <tr>
+                <td>
+                    <div class="cart-info">
+                        <img src="images/readers-republic-logo.png">
+                        <div>
+                            <p>Example Image</p>
+                            <small>Price: £7.50</small><br>
+                            <a href="">Remove</a>
+                        </div>
+                    </div>
+                </td>
+                <td><input type="number" value="1"></td>
+                <td>£7.50</td></td>
+            </tr>
+
+        </table>
+
+            <div class="total-price">
+                <table>
+                    <tr>
+                        <td>Subtotal</td>
+                        <td>£22.50</td>
+                    </tr>
+                    <tr>
+                        <td>Tax</td>
+                        <td>£2.50</td>
+                    </tr>
+                    <tr>
+                        <td>Total</td>
+                        <td>£25.00</td>
+                    </tr>
+                    
+                </table>
+            </div>
+            
+    </div>
 
 @endsection()
