0ffd888f33d466525f80d1758d898497f992923b
spunsosig <ikram15@live.co.uk>
2023-02-23 17:37:32 +0000

    Cart controller bug fixed where item wouldnt be added

--- ecommerce-platform/app/Http/Controllers/CartController.php
+++ ecommerce-platform/app/Http/Controllers/CartController.php
@@ -10,20 +10,20 @@ use App\Models\Cart;
 class CartController extends Controller
 {
     function addToCart (Request $request, $id){
-        if (Auth::login($user)) {
+        $user = Auth::user();
+        if ($user) {
             $product = product::find($id);
             $cart = new cart;
-        
             $cart->product_id=$product->id;
             $cart->user_id = $user->id;
-            //cart->quantity = $request->quantity;
+            $cart->quantity = $request->quantity;
             //$cart->price = $product->price;
             $cart->save();
         
             return redirect()->back()->with('status', 'Product added to Cart!');
         } else {
             echo("user not authenticated");
-            //return redirect('login');
+                return redirect('login');
         }
         
 
