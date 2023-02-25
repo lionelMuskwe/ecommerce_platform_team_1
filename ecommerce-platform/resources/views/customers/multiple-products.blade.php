9f2d5b955f6fd317857038175e6b10c503e15922
Kesh0707 <keshavparikh18@gmail.com>
2023-02-25 17:27:56 +0000

    Changes made to Products Page

--- ecommerce-platform/resources/views/customers/multiple-products.blade.php
+++ ecommerce-platform/resources/views/customers/multiple-products.blade.php
@@ -6,13 +6,26 @@
 
 @section("links")
     <link rel="stylesheet" href="{{asset('css/multiple-products.css')}}">
+    <a href="Downloads/Blank-Image.jpg">
 @endsection
 
 @section("content")
+<!DOCTYPE html>
+<html>
+<head>/
+    <title>Product Page</title>
+</head>
+<body>
+    <div class="product-container">
+        <img src="Blank-Image.jpg" alt="Image" class= "product-image">
     {{-- Content goes in this section --}}
     <h1>You content should go in this section</h1>
     <br>
     <p>Remember to always write clean code</p>
+
+
+
+</body>
 
 @endsection()
     
