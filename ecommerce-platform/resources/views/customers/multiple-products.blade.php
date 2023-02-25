701ecd8cee95cdc63f5ab7f638547529526a7ed4
Kesh0707 <keshavparikh18@gmail.com>
2023-02-25 17:52:34 +0000

    Changes to Product Page

--- ecommerce-platform/resources/views/customers/multiple-products.blade.php
+++ ecommerce-platform/resources/views/customers/multiple-products.blade.php
@@ -14,30 +14,31 @@ Products
 
 <h1> Products </h1>
 @foreach ($products as $product)
-<a href="{{ route('product.detail', ['id' => $product['id']]) }}">{{$product['title']}}</a>
+    <a href="{{ route('product.detail', ['id' => $product['id']]) }}">{{$product['title']}}</a>
 
-<form action="{{ url('add-to-cart', $product->id) }}" method="POST">
-    @csrf
-    <input type="number" value="1" min="1" class="form-control" style="width: 60px;height:35px; text-align:center"
-        name="quantity">
-    <p><button type ="submit" >Add to Cart</button></p>
-</form>
-<!DOCTYPE html>
-<html>
-<head>/
-    <title>Product Page</title>
-</head>
-<body>
     <div class="product-container">
-        <img src="Blank-Image.jpg" alt="Image" class= "product-image">
-    {{-- Content goes in this section --}}
-    <h1>You content should go in this section</h1>
-    <br>
-    <p>Remember to always write clean code</p>
+        <div class="product-information">
+            <img src="Blank-Image.jpg" alt="Image" class= "product-image">
+            {{-- Content goes in this section --}}
+            <div class="product-title">
+                {{$product['title']}}
+            </div>
+            <div class="product-description">
+                {{$product['description']}}
+            </div>
+            
+        </div>
 
+        <div>
+            <form action="{{ url('add-to-cart', $product->id) }}" method="POST">
+                @csrf
+                <input type="number" value="1" min="1" class="form-control" style="width: 60px;height:35px; text-align:center"
+                    name="quantity">
+                <p><button type ="submit" >Add to Cart</button></p>
+            </form>
+        </div>
+    </div>
 @endforeach
 
-
-</body>
 
 @endsection()
