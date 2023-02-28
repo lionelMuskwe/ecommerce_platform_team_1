dee1e32b8bc8023581c0385973d37c3f0171ad30
spunsosig <ikram15@live.co.uk>
2023-02-28 20:27:44 +0000

    Added checkout button to cart

--- ecommerce-platform/resources/views/general/user-basket.blade.php
+++ ecommerce-platform/resources/views/general/user-basket.blade.php
@@ -63,8 +63,10 @@
                         <td>Total</td>
                         <td>£{{number_format($total, 2)}} </td>
                     </tr>
-                    
                 </table>
+            </div>
+            <div>
+                <a href="home"><button>Proceed to Checkout</button></a>
             </div>
             
     </div>
