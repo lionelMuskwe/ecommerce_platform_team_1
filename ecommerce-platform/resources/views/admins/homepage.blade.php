2103ae05bb4fa945619c5d717946ae881b5aa53d
David David-Aghaiyo <210186620@aston.ac.uk>
2023-03-07 12:23:32 +0000

    remodified the admin homepage and the css

--- ecommerce-platform/resources/views/admins/homepage.blade.php
+++ ecommerce-platform/resources/views/admins/homepage.blade.php
@@ -9,41 +9,67 @@ Admin Home
 @endsection
 
 @section("content")
-<div class="container-fluid banner">
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top">
+    <a class="navbar-brand ml-5 font-weight-bold" href="#">Readers Republic</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
 
-    <div class="row">
-        <div class="col-md-12">
-            <nav class="navbar navbar-md">
-                <div class="navbar-brand">
-                    READERS REPUBLIC
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto mr-5 pr-5">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false">
+                    Products
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Add Product</a>
+                    <a class="dropdown-item" href="#">Product List</a>
+                    <!-- <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Something else here</a> -->
                 </div>
-                <ul class="nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">HOME</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#"> ADD PRODUCTS</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">ADD EMPLOYEE</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">ORDERS</a>
-                    </li>
-                </ul>
-            </nav>
-
-        </div>
-        <div class="col-md-8 offset-md-2 info">
-            <h1 class="text-center">READERS REPUBLIC</h1>
-            <p class="text-center">
-                Welcome Admin
-            </p>
-            <!-- <a href="#" class="btn btn-md text-center">GET STARTED</a> -->
-
-        </div>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false">
+                    Employees
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Add Employee</a>
+                    <a class="dropdown-item" href="#">Employee List</a>
+                    <!-- <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Something else here</a> -->
+                </div>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false">
+                    Orders
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Order List</a>
+                    <a class="dropdown-item" href="#">Complaints</a>
+                    <!-- <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Something else here</a> -->
+                </div>
+            </li>
+        </ul>
+        <!-- <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form> -->
     </div>
+</nav>
+<div style="margin-top:23%; text-align:center; color:white;">
+    <h1 class="font-weight-bold" style="font-size: 50px">WELCOME ADMIN</h1>
+    <h4>This is the admin page of Readers Republic</h4>
 </div>
+
+
 
 
 
--- ecommerce-platform/resources/views/general/admin-master.blade.php
+++ ecommerce-platform/resources/views/general/admin-master.blade.php
@@ -12,6 +12,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/effeb5c9fd.js" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.2.0/mdb.min.js"></script>
 
     @yield("links")
 
@@ -25,6 +26,6 @@
     </div>
 
 
-    @include("general/footer")
+    <!-- @include("general/footer") -->
 </body>
 </html>
