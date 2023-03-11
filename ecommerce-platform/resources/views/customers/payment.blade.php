01ecf751c5b2367bb65d5ec0ba7a3e3313b2cd65
Onno Tsang <onno7@outlook.com>
2023-03-11 10:20:12 +0000

    Uodated payment page to include validation for the data

--- ecommerce-platform/resources/views/customers/payment.blade.php
+++ ecommerce-platform/resources/views/customers/payment.blade.php
@@ -13,16 +13,43 @@ Payment
 
 <body>
 
-    <form action="" method="POST">
+    <form action="/paymentDetails" method="POST" id="paymentForm">
     @csrf
     <label for="">Card Number</label>
-    <input type="text" name="account_number" placeholder="Card Number"><br>
-    <label for="">Sort Code</label>
-    <input type="text" name="sort_code" placeholder="Enter Sort Code"><br>
+    <input type="text" name="account_number" id="account_number" placeholder="Card Number"><br>
+    <label for="">CVV</label>
+    <input type="text" name="cvv" id="cvv" placeholder="cvv"><br>
     <label for="">Expiration Date</label>
-    <input type="text" name="expiration_date"><br>
+    <input type="text" name="expiration_date" id = "expiration_date"><br>
     <button type="submit">Checkout</button>
     </form>
+
+    <script>
+        const paymentForm = document.getElementById("paymentForm");
+
+        paymentForm.addEventListener("submit", function(event){
+            event.preventDefault();
+
+            const accountNumber = document.getElementById("account_number").value;
+            const cvvCode = document.getElementById("cvv").value;
+            const expirationDate = document.getElementById("expiration_date").value;
+
+            const accountNumberRegex = /^[0-9]{16}$/;
+            const cvvRegex = /^[0-9]{3}$/;
+
+            if (!accountNumberRegex.test(accountNumber)) {
+              alert("Card Number must contain 16 digits");
+              return false;
+            }
+
+            if (!cvvRegex.test(cvvCode)) {
+              alert("CVV number must contain 3 digits");
+              return false;
+            }
+
+            window.location.href="/home";
+        });
+    </script>
 
 </body>
 
