780d48f04bcbfb6a395103955bedc66876a95647
lionelMuskwe <lionel04muskwe@gmail.com>
2023-03-13 15:06:11 +0000

    Added confirmation page function

--- ecommerce-platform/app/Http/Controllers/OrderItemController.php
+++ ecommerce-platform/app/Http/Controllers/OrderItemController.php
@@ -7,4 +7,11 @@ use Illuminate\Http\Request;
 class OrderItemController extends Controller
 {
     //
+    public function show(Request $request)
+    {
+        $user = $request->user();
+        // Todo: generate random 13 digit order number
+        // Todo: assign random number to current order
+        return view("customers/confirmation-page", compact('user'));
+    }
 }
