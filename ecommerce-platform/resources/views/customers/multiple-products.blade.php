822ccdccaf81a18f51dd47dad743dd56f1ef339e
Kesh0707 <keshavparikh18@gmail.com>
2023-03-14 23:37:49 +0000

    Changes to MPP and Detailed PP css

--- ecommerce-platform/resources/views/customers/multiple-products.blade.php
+++ ecommerce-platform/resources/views/customers/multiple-products.blade.php
@@ -42,22 +42,77 @@ h1 {
                 color: dimgrey
             }
             </style>
+
+            <style>
+            .product-container {
+            position: relative;
+            /* width: 50%; */
+            }
+
+            
+            .product-image {
+                opacity: 1;
+                display: block;
+                transition: .5s ease;
+                backface-visibility: hidden;
+            }
+
+            .middle {
+            transition: .5s ease;
+            opacity: 0;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            -ms-transform: translate(-50%, -50%)
+            }
+
+            .product-container:hover .product-image{
+                opacity: 0.3;
+            }
+
+            .product-container:hover .image {
+            opacity: 0.3;
+            }
+
+            .text {
+            background-color: #4CAF50;
+            color: white;
+            font-size: 16px;
+            padding: 16px 32px;
+            }
+                       
+            </style>
 </body>
 
 @foreach ($products as $product)
     <!--<a href="{{ route('product.detail', ['id' => $product['id']]) }}">{{$product['title']}}</a>-->
 
-    <div class="product-container">
-        <div class="product-information">
-            <img src="{{ asset('images') }}/{{ $product->image }}" alt="Image" class= "product-image" height="250" width="150">
-            {{-- Content goes in this section --}}
+   
+        <div class="product-container">
+    
+
+
+        <div class="col-lg-4">
+                    <div class="product-information">
+                    <img src="{{ asset('images') }}/{{ $product->image }}" alt="Image" class= "product-image" height="250" width="150">
+                    <a href="{{ route('product.detail', ['id' => $product['id']]) }}">{{$product['image']}}</a>
+                    {{-- Content goes in this section --}}
+                    </div>
+                    {{-- <div class="container"
+                        <div class="middle">
+                            <div class="text">TEXT</div>
+                        </div> --}}
+        
+
             <div class="product-title">
-            <a href="{{ route('product.detail', ['id' => $product['id']]) }}">{{$product['title']}}</a>
+                <a href="{{ route('product.detail', ['id' => $product['id']]) }}">{{$product['title']}}</a>
             </div>
+
             <div class="product-price">
-                £{{$product['price']}}
+                    £{{$product['price']}}
             </div>
-        </div>
+    
 
         <div>
             <form action="{{ url('add-to-cart', $product->id) }}" method="POST">
@@ -68,6 +123,10 @@ h1 {
             </form>
             <div class="icons"<i class="bi bi-cart4"></i></div>
         </div>
+        </div>
     </div>
+
+
+
 @endforeach
 @endsection()
