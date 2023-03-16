dfeefe33c4074bf504ba2e72c2a2c940063ece90
Kesh0707 <keshavparikh18@gmail.com>
2023-03-16 16:07:51 +0000

    multiple product page completed, filter feature added

--- ecommerce-platform/resources/views/customers/multiple-products.blade.php
+++ ecommerce-platform/resources/views/customers/multiple-products.blade.php
@@ -16,14 +16,24 @@ h1 {
   }
 </style>
 
+
+<div class="filter">
+
         <a href = "{{route('multiple-products')}}"><button>All</button></a>
 @foreach($categories as $genre)
+    
         <a href = "{{route('product.filter', ['id' => $genre['id']])}}"><button>{{$genre['name']}}</button></a>
 @endforeach
+
+</div>
 
 
 <body>
             <style>
+
+            body{
+            background-color:whitesmoke;
+            }
             a:link {
               color: black;
             }
@@ -36,6 +46,7 @@ h1 {
             </style>
 
             <style>
+
             .product-container {
             position: relative;
             /* width: 50%; */
@@ -82,22 +93,15 @@ h1 {
     <!--<a href="{{ route('product.detail', ['id' => $product['id']]) }}">{{$product['title']}}</a>-->
 
    
-        <div class="product-container">
     
-
-
-        <div class="col-lg-4">
+    <div class="product-container">
+    
+        <div class="product">
                     <div class="product-information">
                     <a href="{{ route('product.detail', ['id' => $product['id']]) }}"><a href="{{ route('product.detail', ['id' => $product['id']]) }}"><img src="{{ asset('images') }}/{{ $product->image }}" alt="Image" class= "product-image" height="250" width="150"></a>
                     
                     {{-- Content goes in this section --}}
                     </div>
-                    {{-- <div class="container"
-                        <div class="middle">
-                            <div class="text">TEXT</div>
-                        </div> --}}
-        
-
             <div class="product-title">
                 <a href="{{ route('product.detail', ['id' => $product['id']]) }}">{{$product['title']}}</a>
             </div>
@@ -110,12 +114,16 @@ h1 {
         <div>
             <form action="{{ url('add-to-cart', $product->id) }}" method="POST">
                 @csrf
-                <input type="number" value="1" min="1" class="form-control" style="width: 60px;height:35px; text-align:center"
+                <div class="quantity">
+                    <input type="number" value="1" min="1" class="form-control"
                     name="quantity">
+                </div>
+
                 <p><button class ="submit"><span>Add to Cart</span></button></p>
             </form>
             <div class="icons"<i class="bi bi-cart4"></i></div>
         </div>
+        
         </div>
     </div>
 
