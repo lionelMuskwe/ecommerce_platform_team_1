31d7ef781f6a0884005555f7350dd0a6ae84b34d
junnayd <junnayd@icloud.com>
2023-03-18 01:35:34 +0000

    updated detailed product page

--- ecommerce-platform/resources/views/customers/detailed-product.blade.php
+++ ecommerce-platform/resources/views/customers/detailed-product.blade.php
@@ -1,56 +1,37 @@
 @extends("../general/master")
 
 @section("title")
-    Product Details
+    Products
 @endsection()
 
 @section("links")
-    <link rel="stylesheet" href="{{asset('css/detailed-product.css')}}">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="{{asset('css/products.css')}}">
 @endsection
 
 @section("content")
-    {{-- Content goes in this section --}}
-
-<div class="centre">
-    <div class="card">
-        <img src="{{ asset('images') }}/{{ $products->image}}" alt="" height="450" width="350">
-        <div class="card-title">
-            <h3>{{$products['title']}}</h3>
-        </div>
-        <div class="submit">
-            {{-- <form action="{{ url('add-to-cart', $product->id) }}" method="POST"> --}}
-                <form>
-                @csrf
-                <input type="number" value="1" min="1" class="form-control" style="width: 60px;height:35px; text-align:center"
-                    name="quantity">
-                <p><button class ="submit"><span>Add to Cart</span></button></p>
-            </form>
-            <div class="icons"<i class="bi bi-cart4"></i></div>
-            <div class="details">
-                <p>Price: £{{$products['price']}}</p>
-                <p>Author: {{$products['author']}}</p>
-                <p>Description: {{$products['description']}}</p>
+<div class="container">
+    <div class="row">
+        @foreach ($products as $product)
+            <div class="col-lg-4 col-md-6 col-sm-12">
+                <div class="product-card">
+                    <img class="product-image" src="{{ asset('images') }}/{{ $product->image }}" alt="" height="200" width="150">
+                    <div class="product-info">
+                        <h3 class="product-title">{{ $product['title'] }}</h3>
+                        <p class="product-author">Author: {{ $product['author'] }}</p>
+                        <p class="product-price">Price: £{{ $product['price'] }}</p>
+                        <div class="dropdown">
+                            <button class="dropdown-btn">Details</button>
+                            <div class="dropdown-content">
+                                <p>Description: {{ $product['description'] }}</p>
+                                <p>ISBN: {{ $product['ISBN'] }}</p>
+                                <p>Publisher: {{ $product['publisher'] }}</p>
+                                <p>Date Published: {{ $product['date_published'] }}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
+        @endforeach
     </div>
 </div>
-
-
-
-
-
-
-
-<h1>{{$products['title']}}</h1>
-<img src="{{ asset('images') }}/{{ $products->image}}" alt="" height="250" width="150">
-<p>ISBN: {{$products['ISBN']}}</p>
-<p>Price: £{{$products['price']}}</p>
-<p>Author: {{$products['author']}}</p>
-<p>Description: {{$products['description']}}</p>
-<p>Date Published: {{$products['date_published']}}</p>
-<p>Publisher: {{$products['publisher']}}</p>
-
-
-@endsection()
-
+@endsection
