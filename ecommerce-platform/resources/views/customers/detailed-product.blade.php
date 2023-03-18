4aca27f5020ca45f7c41ec8e3216b5ba9b19e79b
Kesh0707 <keshavparikh18@gmail.com>
2023-03-18 12:43:51 +0000

    detailed products page ready for review

--- ecommerce-platform/resources/views/customers/detailed-product.blade.php
+++ ecommerce-platform/resources/views/customers/detailed-product.blade.php
@@ -10,46 +10,65 @@
 @endsection
 
 @section("content")
+
+
+<body>
+    <style>
+
+    body{
+    background-color:whitesmoke;
+    font-family: Arial, Helvetica, sans-serif;
+    box-sizing: border-box;
+    margin: 0;
+    padding:0;
+    background: lightgray;
+    }
+               
+    </style>
+</body>
+
+
     {{-- Content goes in this section --}}
 
-<div class="centre">
-    <div class="card">
-        <img src="{{ asset('images') }}/{{ $products->image}}" alt="" height="450" width="350">
-        <div class="card-title">
-            <h3>{{$products['title']}}</h3>
-        </div>
-        <div class="submit">
-            {{-- <form action="{{ url('add-to-cart', $product->id) }}" method="POST"> --}}
-                <form>
-                @csrf
-                <input type="number" value="1" min="1" class="form-control" style="width: 60px;height:35px; text-align:center"
-                    name="quantity">
-                <p><button class ="submit"><span>Add to Cart</span></button></p>
-            </form>
-            <div class="icons"<i class="bi bi-cart4"></i></div>
-            <div class="details">
-                <p>Price: £{{$products['price']}}</p>
-                <p>Author: {{$products['author']}}</p>
-                <p>Description: {{$products['description']}}</p>
+    <div class="product-container">
+
+        <div class="centre">
+            <div class="card">
+                <img src="{{ asset('images') }}/{{ $products->image}}" alt="" height="450" width="350">
+                <div class="card-title">
+                    <h3>View details</h3>
+                </div>
+                <div class="card-details">
+                    <h1>{{$products['title']}}</h1>
+                    <p>Price: £{{$products['price']}}</p>
+                    <p>Author: {{$products['author']}}</p>
+                    <p>ISBN: {{$products['ISBN']}}</p>
+                    <p>Date Published: {{$products['date_published']}}</p>
+                    <p>Publisher: {{$products['publisher']}}</p>
+                </div>
+
+                
+
             </div>
         </div>
+
+                <div class="product-details">
+                    <p>{{$products['description']}}</p>
+                    <p>Price: £{{$products['price']}}</p>
+                    <p>Author: {{$products['author']}}</p>
+                    <div class="submit"
+                    <form>
+                        <p><button class ="submit"><span>Add to Cart</span></button></p>
+                    </form>
+            </div>
+        </div>
+
+
     </div>
-</div>
 
 
 
 
-
-
-
-<h1>{{$products['title']}}</h1>
-<img src="{{ asset('images') }}/{{ $products->image}}" alt="" height="250" width="150">
-<p>ISBN: {{$products['ISBN']}}</p>
-<p>Price: £{{$products['price']}}</p>
-<p>Author: {{$products['author']}}</p>
-<p>Description: {{$products['description']}}</p>
-<p>Date Published: {{$products['date_published']}}</p>
-<p>Publisher: {{$products['publisher']}}</p>
 
 
 @endsection()
