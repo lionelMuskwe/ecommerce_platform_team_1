bd22d6f57058dc397c1fdd75cd649c941e606617
lionelMuskwe <lionel04muskwe@gmail.com>
2023-03-26 07:35:15 +0100

    Removed order_items_id field, added set timestamps to false

--- ecommerce-platform/app/Models/OrderDetail.php
+++ ecommerce-platform/app/Models/OrderDetail.php
@@ -8,11 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class OrderDetail extends Model
 {
     use HasFactory;
+    public $timestamps = false;
 
     protected $fillable = [
 
         "user_id",
-        "order_items_id",
         "total",
         "status",
         "confirmation_id",
