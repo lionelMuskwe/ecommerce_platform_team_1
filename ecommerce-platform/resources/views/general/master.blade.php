61005552b76209b4c1a9968b77703d905c539fc2
lionelMuskwe <lionel04muskwe@gmail.com>
2023-03-26 09:47:11 +0100

    Snackbar can now be used on any customer pages

--- ecommerce-platform/resources/views/general/master.blade.php
+++ ecommerce-platform/resources/views/general/master.blade.php
@@ -9,6 +9,8 @@
     </title>
     {{-- Below are links to BOOTSTRAP resources + fontawesome --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{asset('css/snackbar.css')}}">
+    <script defer src="{{asset('js/snackbar.js')}}"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/effeb5c9fd.js" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
@@ -24,7 +26,9 @@
       @yield('content')
     </div>
 
-
+    <span hidden id="snack-bar-colour">{{ Session::get('alert-class')}}</span>
+    <div id="snackbar">{{ Session::get('message')}}</div>
+    
     @include("general/footer")
 </body>
 </html>
