68283475660b45bf72de3a68dfb33dfc4b6505a6
Kesh0707 <keshavparikh18@gmail.com>
2023-03-27 15:12:26 +0100

    fixes to detailed product page

--- ecommerce-platform/resources/views/customers/detailed-product.blade.php
+++ ecommerce-platform/resources/views/customers/detailed-product.blade.php
@@ -5,7 +5,8 @@
 @endsection()
 
 @section("links")
-    <link rel="stylesheet" href="{{asset('css/products.css')}}">
+    <link rel="stylesheet" href="{{asset('css/detailed-product.css')}}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
 @endsection
 
 @section("content")
@@ -21,6 +22,7 @@
     margin: 0;
     padding:0;
     background: lightgray;
+    
     }
                
     </style>
@@ -34,9 +36,11 @@
         <div class="centre">
             <div class="card">
                 <img src="{{ asset('images') }}/{{ $products->image}}" alt="" height="450" width="350">
+                
                 <div class="card-title">
                     <h3>View details</h3>
                 </div>
+                
                 <div class="card-details">
                     <h1>{{$products['title']}}</h1>
                     <p>Price: £{{$products['price']}}</p>
@@ -55,10 +59,22 @@
                     <p>{{$products['description']}}</p>
                     <p>Price: £{{$products['price']}}</p>
                     <p>Author: {{$products['author']}}</p>
-                    <div class="submit"
+
+                    <div class="submit">
                     <form>
-                        <p><button class ="submit"><span>Add to Cart</span></button></p>
+                        {{-- <form action="{{ url('add-to-cart', $product->id) }}" method="POST"> --}}
+                            @csrf
+                            <div class="quantity">
+                                <input type="number" value="1" min="1" class="form-control"
+                                name="quantity">
+                            </div>
+            
+                            <p><button class ="submit"><span>Add to Cart</span></button></p>
+                        </form>
+                        <div class="icons"<i class="bi bi-cart4"></i></div>
+                        
                     </form>
+                    </div>
             </div>
         </div>
 
