4e3bb2d92211778d2e36f9d0cfd64a2169bc5985
Onno Tsang <onno7@outlook.com>
2023-03-27 17:32:42 +0100

    added a employeePage

---
+++ ecommerce-platform/resources/views/admins/employeePage.blade.php
@@ -0,0 +1,41 @@
+@extends("../general/admin-master")
+
+@section("title")
+    Employees List
+@endsection()
+
+@section("links")
+    <link rel="stylesheet" href="{{asset('css/admin-homepage.css')}}">
+@endsection
+
+@section("content")
+    {{-- Content goes in this section --}}
+<h1>User List</h1>
+
+<table border="1">
+    <tr>
+        <td>Id</td>
+        <td>Username</td>
+        <td>Firstname</td>
+        <td>Lastname</td>
+        <td>Age</td>
+        <td>Address</td>
+        <td>Telephone</td>
+        <td>Role</td>
+    </tr>
+    @foreach($employees as $user)
+    <tr>
+        <td>{{$user['id']}}</td>
+        <td>{{$user['username']}}</td>
+        <td>{{$user['firstname']}}</td>
+        <td>{{$user['lastname']}}</td>
+        <td>{{$user['age']}}</td>
+        <td>{{$user['address']}}</td>
+        <td>{{$user['telephone']}}</td>
+        <td>{{$user['role']}}</td>
+    </tr>
+    @endforeach
+</table>
+@endsection()
+
+
