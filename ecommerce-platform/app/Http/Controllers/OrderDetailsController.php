6cd05f757d283ce6fab61bbb2626c05f3aa9fdd1
Onno Tsang <onno7@outlook.com>
2023-03-28 18:57:46 +0100

    added a controller for orderDetails

--- ecommerce-platform/app/Http/Controllers/OrderDetailsController.php
+++ ecommerce-platform/app/Http/Controllers/OrderDetailsController.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\OrderDetail;
+use App\Models\OrderItem;
 
 class OrderDetailsController extends Controller
 {
-    //
+        public function showOrderDetails()
+        {
+            $orderDetails = OrderDetail::all();
+            return view("admins/orderList", compact('orderDetails'));
+        }
+
+        public function detail($id)
+        {
+
+            $data = OrderItem::whereIn('order_details_id', [$id])->get();
+            
+            return view('admins/orderItem', ['orderItems' => $data]);
+        }
+
+
 }
