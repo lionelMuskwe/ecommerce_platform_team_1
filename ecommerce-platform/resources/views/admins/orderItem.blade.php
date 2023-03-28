8696f770b3c867a3fb2a118b9768dbbcdb52d03a
Onno Tsang <onno7@outlook.com>
2023-03-28 19:15:37 +0100

    Made tables for data

--- ecommerce-platform/resources/views/admins/orderItem.blade.php
+++ ecommerce-platform/resources/views/admins/orderItem.blade.php
@@ -12,10 +12,22 @@
     {{-- Content goes in this section --}}
 <h1>Order Item</h1>
 
-@foreach ($orderItems as $item)
-    <p>Id: {{$item['id']}}</p>
-    <p>Product ID: {{$item['product_id']}}</p>
-    <p>Quantity: {{$item['quantity']}}</p>
-@endforeach
+
+
+    <table border = "1">
+        <tr>
+            <td>ID</td>
+            <td>Product ID</td>
+            <td>Quantity</td>
+        </tr>
+        @foreach ($orderItems as $item)
+        <tr>
+            <td>{{$item['id']}}</td>
+            <td>{{$item['product_id']}}</td>
+            <td>{{$item['quantity']}}</td>
+        </tr>
+        @endforeach
+    </table>
+
 
 @endsection()
