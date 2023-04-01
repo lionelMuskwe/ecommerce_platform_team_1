06a28adf69df22c290c47fa087b69e4525a9003e
junnayd <junnayd@icloud.com>
2023-04-01 21:01:23 +0100

    fixed content and padding made responsive

--- ecommerce-platform/resources/views/customers/about-us.blade.php
+++ ecommerce-platform/resources/views/customers/about-us.blade.php
@@ -9,18 +9,16 @@ About Us
 @endsection
 
 @section("content")
-
-<body>
     <section id="about">
         <div class="about-1">
             <h1>ABOUT US</h1>
             <p>Welcome to Readers Republic! We are a team of book lovers dedicated to providing you with an unparalleled
                 shopping experience for you and all your reading needs. Our passion for books and reading led us to
-                create this platform where you can easily browse, discover and purhcase your favourite titles from the
+                create this platform where you can easily browse, discover and purchase your favorite titles from the
                 comfort of your own home. We offer an extensive selection of books across various genres, including
                 fiction, non-fiction, children's books, textbooks and more.</p>
         </div>
-        <div id="about-2">
+        <div class="about-2">
             <div class="content-box-lg">
                 <div class="container">
                     <div class="row">
@@ -31,9 +29,9 @@ About Us
                                 <hr>
                                 <p>Our mission is to provide a convenient and reliable online bookstore that offers an
                                     extensive selection of books at competitive prices. We strive to create a welcoming
-                                    and and engaging environment that inspires a love of readingand encourages personal
+                                    and engaging environment that inspires a love of reading and encourages personal
                                     growth and education. We believe that books have the power to enrich our lives and
-                                    connect us to new ideas, perspectives and experiences. Therefore we are committed to
+                                    connect us to new ideas, perspectives, and experiences. Therefore, we are committed to
                                     curating a diverse collection of books that appeals to a broad range of readers,
                                     from young children to avid readers and scholars.</p>
                             </div>
@@ -43,13 +41,13 @@ About Us
                                 <i class="fa fa-globe"></i>
                                 <h3>VISION</h3>
                                 <hr>
-                                <p>Our Vision is to be the premier destination for book lovers everywhere, providing an
+                                <p>Our vision is to be the premier destination for book lovers everywhere, providing an
                                     unparalleled online shopping experience for all reading enthusiasts. We strive to be
-                                    recognized as the leading ecommerce bookstore offering a vast and diverse selection
-                                    of books, excellent customer service and innovative digital experiences that enhance
+                                    recognized as the leading e-commerce bookstore offering a vast and diverse selection
+                                    of books, excellent customer service, and innovative digital experiences that enhance
                                     the joy of reading. We envision a future where our bookstore is synonymous with
-                                    quality, relaibility and accessibility. Our goal is to inpire a love of reading and
-                                    learning among people of all ages and backgrounds making books more accessible to
+                                    quality, reliability, and accessibility. Our goal is to inspire a love of reading and
+                                    learning among people of all ages and backgrounds, making books more accessible to
                                     those who might not otherwise have access to them.</p>
                             </div>
                         </div>
@@ -58,7 +56,7 @@ About Us
                                 <i class="fa fa-pencil"></i>
                                 <h3>ACHIEVEMENTS</h3>
                                 <hr>
-                                <p>Some of our achievements include:</br>
+                                <p>Some of our achievements include:</p>
                                 <ul>
                                     <li>Served millions of customers</li>
                                     <li>Provide a vast variety of books and accessories</li>
@@ -66,17 +64,11 @@ About Us
                                     <li>Innovative digital experiences</li>
                                     <li>Award for most innovative new business</li>
                                 </ul>
-                                </p>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-
         </div>
     </section>
-</body>
-
-
-
 @endsection
